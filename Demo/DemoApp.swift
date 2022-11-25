5a94d64fd498d4be8319efe32a74b3ab7996f848
peter <apppeterpan@gmail.com>
2022-11-25 15:06:22 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Peter Pan on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
